146a618a8e31cf24fed90f78a1ce6bd60c93516a
leona <zcon.leona@gmil.com>
2020-04-13 15:29:45 +0800

    Initial commit

---
+++ src/main/java/com/leona/Person.java
@@ -0,0 +1,7 @@
+package com.leona;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello World !!");
+    }
+}
---
+++ src/main/kotlin/com/leona/Hello.kt
@@ -0,0 +1,13 @@
+package com.leona
+
+fun main(args: Array<String>) {
+//    Human().hello();
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello kotlin !!")
+    }
+}
